fcea453b402d446af5831495af079a1e94a14da3
eduardburlacu <efb48@cam.ac.uk>
2024-10-13 17:45:26 +0100

    add and test utils from diffsep

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,3 +3,10 @@ from src.utils.logging_utils import log_hyperparameters
 from src.utils.pylogger import RankedLogger
 from src.utils.rich_utils import enforce_tags, print_config_tree
 from src.utils.utils import extras, get_metric_value, task_wrapper
+from src.utils import ddp, linalg
+from src.utils.autoclip_module import AutoClipper, FixedClipper, grad_norm
+from src.utils.bn_update import bn_update
+from src.utils.checkpoint_symlink import monkey_patch_add_best_symlink, symlink_force
+from src.utils.import_module import import_name, module_from_config, run_configured_func
+from src.utils.processing_pool import ProcessingPool, SyncProcessingPool
+from src.utils.registry import Registry
